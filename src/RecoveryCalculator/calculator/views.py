034b94f56e34fb5f6064f88c0af3c3781b9c06e8
computer-geek64 <sudouser512@gmail.com>
2020-03-28 18:46:20 -0400

    Update

--- src/RecoveryCalculator/calculator/views.py
+++ src/RecoveryCalculator/calculator/views.py
@@ -21,24 +21,6 @@ def geolocate(request, ip):
     return HttpResponse(str(json_object))
 
 
-def get_mortality_rate(request, ip):
-    response = requests.get('http://ip-api.com/json/' + ip)
-    if not response.status_code == 200:
-        return HttpResponse('HTTP 500 Error')
-    ip_data = json.loads(response.text)
-    if ip_data['country'] == 'United States':
-        process = Popen(['../data/worldometers_webscraper.rb'], stdout=PIPE, stderr=PIPE)
-        mortality_rate_data = json.loads(process.communicate()[0].decode().strip())
-        if ip_data['regionName'] in mortality_rate_data['states']:
-            state_index = mortality_rate_data['states'].index(ip_data['regionName'])
-            mortality_rate = mortality_rate_data['deaths'][state_index] / mortality_rate_data['cases'][state_index]
-            return HttpResponse('Mortality rate in ' + ip_data['regionName'] + ': ' + str(round(mortality_rate * 100, 2)) + "%")
-        else:
-            pass
-            # Get location from all of US
-    return HttpResponse('Fix this!')
-
-
 def update_databases(request, **kwargs):
     dataset.download_us_states_mortality_rates_dataset()
     dataset.download_country_mortality_rates_dataset()
