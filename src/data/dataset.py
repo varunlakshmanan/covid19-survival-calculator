dca2ea136be3b9c2c43f215ec39909f25ed6b9ff
computer-geek64 <sudouser512@gmail.com>
2020-03-28 17:14:58 -0400

    Update

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -34,10 +34,10 @@ def download_country_mortality_rates_dataset():
 def merge_mortality_rates_datasets():
     df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'datasets', 'country_mortality_rates.csv'))
     df2 = pd.read_csv(os.path.join(os.path.dirname(__file__), 'datasets', 'us_states_mortality_rates.csv'))
-    df = df.drop(df.loc[df['country'] == 'US'].index)
+    df = df.drop(df.loc[df['country'] == 'us'].index)
     df = df.reset_index(drop=True)
     for i in df2.index:
-        df = df.append(pd.Series(['US', df2['state'][i], df2['cases'][i], df2['deaths'][i], df2['mortality_rate'][i]], index=['country', 'state', 'cases', 'deaths', 'mortality_rate']), ignore_index=True)
+        df = df.append(pd.Series(['us', df2['state'][i], df2['cases'][i], df2['deaths'][i], df2['mortality_rate'][i]], index=['country', 'state', 'cases', 'deaths', 'mortality_rate']), ignore_index=True)
     df = df.sort_values(['country', 'state'])
     df.to_csv(os.path.join(os.path.dirname(__file__), 'datasets', 'mortality_rates.csv'), index=False)
 
@@ -68,7 +68,7 @@ def merge_into_original_dataset():
         if df['country'][i] in df2['country'].values:
             series.append(df2.loc[df2['country'] == df['country'][i]].iloc[0]['pop_density'])
         else:
-            series.append(25) # Worldwide population density
+            series.append(25)  # Worldwide population density
     series = pd.Series(series)
     df['pop_density'] = series
     df.to_csv(os.path.join(os.path.dirname(__file__), 'datasets', 'data.csv'), index=False)
