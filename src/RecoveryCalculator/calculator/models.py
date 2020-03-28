0fbd4b484950036cf286bee71d9c0747e103ce27
computer-geek64 <sudouser512@gmail.com>
2020-03-28 18:30:21 -0400

    Update

--- src/RecoveryCalculator/calculator/models.py
+++ src/RecoveryCalculator/calculator/models.py
@@ -1,4 +1,53 @@
 from django.db import models
+import os
+import pandas as pd
 
 
-#class Person(models.Model):
+class Person(models.Model):
+    location = models.TextField()
+    country = models.TextField()
+    age = models.IntegerField()
+    death = models.BooleanField()
+    male = models.BooleanField()
+    female = models.BooleanField()
+    symptom_onset_hospitalization = models.IntegerField()
+    mortality_rate = models.FloatField()
+    pop_density = models.FloatField()
+    high_risk_travel = models.BooleanField()
+
+    def __str__(self):
+        return str(self.location) + ', ' + str(self.country)
+
+    @staticmethod
+    def to_data_frame():
+        people = Person.objects.all()
+        data = {
+            'location': [],
+            'country': [],
+            'age': [],
+            'death': [],
+            'male': [],
+            'female': [],
+            'symptom_onset_hospitalization': [],
+            'mortality_rate': [],
+            'pop_density': [],
+            'high_risk_travel': []
+        }
+        for person in people:
+            data['location'].append(person.location)
+            data['country'].append(person.country)
+            data['age'].append(person.age)
+            data['death'].append(int(person.death))
+            data['male'].append(int(person.male))
+            data['female'].append(int(person.female))
+            data['symptom_onset_hospitalization'].append(person.symptom_onset_hospitalization)
+            data['mortality_rate'].append(person.mortality_rate)
+            data['pop_density'].append(person.pop_density)
+            data['high_risk_travel'].append(int(person.high_risk_travel))
+        return pd.DataFrame(data)
+
+    @staticmethod
+    def to_new_csv():
+        df = pd.read_csv(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'data', 'datasets', 'data.csv'))
+        df2 = Person.to_data_frame()
+        df.append(df2).sort_values(['country', 'location']).reset_index(drop=True).to_csv('new_data.csv', index=False)
--- src/data/dataset.py
+++ src/data/dataset.py
@@ -8,19 +8,19 @@ from subprocess import Popen, PIPE
 
 
 def download_us_states_mortality_rates_dataset():
-    process = Popen([os.path.join(os.path.dirname(__file__), 'worldometers_webscraper.rb')], stdout=PIPE, stderr=PIPE)
+    process = Popen([os.path.join(os.path.dirname(os.path.abspath(__file__)), 'worldometers_webscraper.rb')], stdout=PIPE, stderr=PIPE)
     mortality_rate_data = json.loads(process.communicate()[0].decode().strip())
     df = pd.DataFrame(mortality_rate_data)
     df['mortality_rate'] = pd.Series([df['deaths'][i] / df['cases'][i] if df['cases'][i] > 0 else 0 for i in df.index])
     df['state'] = df['state'].map(lambda x: x.lower() if type(x) == str else x)
-    df.to_csv(os.path.join(os.path.dirname(__file__), 'datasets', 'us_states_mortality_rates.csv'), index=False)
+    df.to_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'datasets', 'us_states_mortality_rates.csv'), index=False)
 
 
 def download_country_mortality_rates_dataset():
     response = requests.get('https://datahub.io/core/covid-19/r/time-series-19-covid-combined.csv')
-    with open(os.path.join(os.path.dirname(__file__), 'datasets', 'country_mortality_rates.csv'), 'wb') as file:
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'datasets', 'country_mortality_rates.csv'), 'wb') as file:
         file.write(response.content)
-    df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'datasets', 'country_mortality_rates.csv'))
+    df = pd.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'datasets', 'country_mortality_rates.csv'))
     df = df.loc[df['Date'] == df['Date'].max()]
     df = df.drop(['Date', 'Lat', 'Long', 'Recovered'], axis=1)
     df = df.rename(columns={'Deaths': 'deaths', 'Confirmed': 'cases', 'Country/Region': 'country', 'Province/State': 'state'})
@@ -28,24 +28,24 @@ def download_country_mortality_rates_dataset():
     df['mortality_rate'] = pd.Series([df['deaths'][i] / df['cases'][i] if df['cases'][i] > 0 else 0 for i in df.index])
     df['country'] = df['country'].map(lambda x: x.lower() if type(x) == str else x)
     df['state'] = df['state'].map(lambda x: x.lower() if type(x) == str else x)
-    df.to_csv(os.path.join(os.path.dirname(__file__), 'datasets', 'country_mortality_rates.csv'), index=False)
+    df.to_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'datasets', 'country_mortality_rates.csv'), index=False)
 
 
 def merge_mortality_rates_datasets():
-    df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'datasets', 'country_mortality_rates.csv'))
-    df2 = pd.read_csv(os.path.join(os.path.dirname(__file__), 'datasets', 'us_states_mortality_rates.csv'))
+    df = pd.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'datasets', 'country_mortality_rates.csv'))
+    df2 = pd.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'datasets', 'us_states_mortality_rates.csv'))
     df = df.drop(df.loc[df['country'] == 'us'].index)
     df = df.reset_index(drop=True)
     for i in df2.index:
         df = df.append(pd.Series(['us', df2['state'][i], df2['cases'][i], df2['deaths'][i], df2['mortality_rate'][i]], index=['country', 'state', 'cases', 'deaths', 'mortality_rate']), ignore_index=True)
     df = df.sort_values(['country', 'state'])
-    df.to_csv(os.path.join(os.path.dirname(__file__), 'datasets', 'mortality_rates.csv'), index=False)
+    df.to_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'datasets', 'mortality_rates.csv'), index=False)
 
 
 def merge_into_original_dataset():
     # Mortality rate
-    df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'datasets', 'data.csv'))
-    df2 = pd.read_csv(os.path.join(os.path.dirname(__file__), 'datasets', 'mortality_rates.csv'))
+    df = pd.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'datasets', 'data.csv'))
+    df2 = pd.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'datasets', 'mortality_rates.csv'))
 
     series = []
     for i in df.index:
@@ -61,7 +61,7 @@ def merge_into_original_dataset():
     df['mortality_rate'] = series
 
     # Population density
-    df2 = pd.read_csv(os.path.join(os.path.dirname(__file__), 'datasets', 'population_density.csv'))
+    df2 = pd.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'datasets', 'population_density.csv'))
 
     series = []
     for i in df.index:
@@ -71,4 +71,4 @@ def merge_into_original_dataset():
             series.append(25)  # Worldwide population density
     series = pd.Series(series)
     df['pop_density'] = series
-    df.to_csv(os.path.join(os.path.dirname(__file__), 'datasets', 'data.csv'), index=False)
+    df.to_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'datasets', 'data.csv'), index=False)
