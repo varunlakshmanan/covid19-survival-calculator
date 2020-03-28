021c8f54c3788bab6bf546ab6fd6ef43cfebc45e
computer-geek64 <sudouser512@gmail.com>
2020-03-28 02:18:43 -0400

    Update

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 import pandas as pd
-import numpy as np
 import json
 import requests
 from subprocess import Popen, PIPE
 
 
-def download_us_states_mortality_rate_dataset():
+def download_us_states_mortality_rates_dataset():
     process = Popen(['./worldometers_webscraper.rb'], stdout=PIPE, stderr=PIPE)
     mortality_rate_data = json.loads(process.communicate()[0].decode().strip())
     df = pd.DataFrame(mortality_rate_data)
@@ -15,7 +14,7 @@ def download_us_states_mortality_rate_dataset():
     df.to_csv('datasets/us_states_mortality_rates.csv', index=False)
 
 
-def download_country_mortality_rate_dataset():
+def download_country_mortality_rates_dataset():
     response = requests.get('https://datahub.io/core/covid-19/r/time-series-19-covid-combined.csv')
     with open('datasets/country_mortality_rates.csv', 'wb') as file:
         file.write(response.content)
@@ -23,22 +22,22 @@ def download_country_mortality_rate_dataset():
     df = df.loc[df['Date'] == df['Date'].max()]
     df = df.drop(['Date', 'Lat', 'Long'], axis=1)
     df = df.rename(columns={'Deaths': 'deaths', 'Confirmed': 'cases', 'Country/Region': 'country', 'Province/State': 'state'})
-    df = df.reset_index()
+    df = df.reset_index(drop=True)
     df['mortality_rate'] = pd.Series([df['deaths'][i] / df['cases'][i] if df['cases'][i] > 0 else 0 for i in df.index])
     df.to_csv('datasets/country_mortality_rates.csv', index=False)
 
 
-def merge_datasets():
+def merge_mortality_rates_datasets():
     df = pd.read_csv('datasets/country_mortality_rates.csv')
     df2 = pd.read_csv('datasets/us_states_mortality_rates.csv')
     df = df.drop(df.loc[df['country'] == 'US'].index)
-    df = df.reset_index()
+    df = df.reset_index(drop=True)
     for i in df2.index:
         df = df.append(pd.Series(['US', df2['states'][i], df2['cases'][i], df2['deaths'][i], df2['mortality_rate'][i]], index=['country', 'state', 'cases', 'deaths', 'mortality_rate']), ignore_index=True)
     df = df.sort_values(['country', 'state'])
     df.to_csv('datasets/mortality_rates.csv', index=False)
 
 
-download_us_states_mortality_rate_dataset()
-download_country_mortality_rate_dataset()
-merge_datasets()
+download_us_states_mortality_rates_dataset()
+download_country_mortality_rates_dataset()
+merge_mortality_rates_datasets()
