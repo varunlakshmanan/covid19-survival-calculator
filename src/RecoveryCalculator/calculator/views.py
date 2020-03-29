18f4769b70cffb803417e4b0895754cc25fd228a
computer-geek64 <sudouser512@gmail.com>
2020-03-29 04:17:12 -0400

    Update

--- src/RecoveryCalculator/calculator/views.py
+++ src/RecoveryCalculator/calculator/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 import os
 import sys
 import json
+import math
+import numpy as np
 import requests
 import pandas as pd
 from subprocess import Popen, PIPE
@@ -31,31 +33,30 @@ def home(request):
 #   medical_conditions: string representing prior medical conditions (see Google Doc for possible choices)
 @csrf_exempt
 def predict(request):
-    ip = ''.join(request.POST.get('ip'))
+    ip = request.POST.get('ip')
     response = requests.get('http://ip-api.com/json/' + ip)
     if not response.status_code == 200:
         return HttpResponse('HTTP 500 Error')
     ip_data = json.loads(response.text)
     region = ip_data['regionName'].lower()
     country = 'us' if ip_data['country'] == 'United States' else ip_data['country'].lower()
-
     data = {
         'region': region,
         'country': country,
-        'age': int(''.join(request.POST.get('age'))),
+        'age': int(request.POST.get('age')),
         'death': 0,
-        'male': 1 - int(''.join(request.POST.get('gender')).lower() in ['female', 'f']),
-        'female': int(''.join(request.POST.get('gender')).lower() in ['female', 'f']),
-        'symptom_onset_hospitalization': int(''.join(request.POST.get('symptom_onset_hospitalization'))),
+        'male': 1 - int(request.POST.get('gender').lower() in ['female', 'f']),
+        'female': int(request.POST.get('gender').lower() in ['female', 'f']),
+        'symptom_onset_hospitalization': int(request.POST.get('symptom_onset_hospitalization')),
         'mortality_rate': dataset.get_mortality_rate(country, region),
         'pop_density': dataset.get_pop_density(country),
-        'high_risk_travel': int(''.join(request.POST.get('high_risk_travel')).lower() in ['yes', 'y'])
+        'high_risk_travel': int(request.POST.get('high_risk_travel').lower() in ['yes', 'y'])
     }
 
     df = pd.DataFrame({k: [v] for k, v in data.items()})
 
     medical_condition_death_rates = {
-        'cardiovascular disease': 0.015,
+        'cardiovascular disease': 0.105,
         'diabetes': 0.073,
         'chronic respiratory disease': 0.063,
         'hypertension': 0.06,
@@ -63,12 +64,14 @@ def predict(request):
         'none': 0.009
     }
 
-    medical_conditions = ''.join(request.POST.get('medical_conditions'))
+    medical_condition_factor = np.prod([medical_condition_death_rates[x] / medical_condition_death_rates['none'] for x in request.POST.getlist('medical_conditions')])
 
-    medical_conditions = medical_condition_death_rates[medical_conditions.lower()] if medical_conditions.lower() in medical_condition_death_rates.keys() else medical_condition_death_rates['none']
+    medical_condition_factor = 200 / (1 + math.exp(-medical_condition_factor / 100)) - 100 if medical_condition_factor > 1 else 1.0  # Modified sigmoid function
+
+    print(medical_condition_factor)
 
     p = Person(**data)
-    p.save()
+    #p.save()
 
     return HttpResponse(str(list(df.values[0])))
 
