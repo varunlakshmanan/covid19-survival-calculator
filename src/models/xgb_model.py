c4ede87ed6b6a725b7a5fd83103dc55eea17598d
varunlakshmanan <varunlakshmanan@outlook.com>
2020-03-29 19:25:06 -0400

    Semantics

--- src/models/xgb_model.py
+++ src/models/xgb_model.py
@@ -1,9 +1,8 @@
-def create_xgboost_model(test_data):
-    import os
-    import pandas as pd
+import os
+import pandas as pd
 
+def create_xgboost_model(test_data):
     data_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'datasets', 'data.csv')
-    #data_file_path = 'D:/Documents/Leetcode + Foobar + Kaggle/COVIDCalculator/updated_data.csv'
     data = pd.read_csv(data_file_path)
 
     features = ['age', 'male', 'female', 'symptom_onset_hospitalization', 'mortality_rate', 'pop_density', 'high_risk_travel']
