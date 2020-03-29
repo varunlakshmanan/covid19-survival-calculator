6a855c7ffebbad8aa8a970183f68f35dce510b6e
varunlakshmanan <varunlakshmanan@outlook.com>
2020-03-29 13:10:52 -0400

    Update xgb_model.py

--- src/models/xgb_model.py
+++ src/models/xgb_model.py
@@ -1,27 +1,24 @@
-import os
-import pandas as pd
+def create_xgboost_model(test_data):
+    import os
+    import pandas as pd
 
-data_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'datasets', 'data.csv')
-data = pd.read_csv(data_file_path)
+    data_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'datasets', 'data.csv')
+    #data_file_path = 'D:/Documents/Leetcode + Foobar + Kaggle/COVIDCalculator/updated_data.csv'
+    data = pd.read_csv(data_file_path)
 
-features = ['age', 'international_traveler', 'domestic_traveler', 'male', 'female', 'number of days between symptom onset and hospitalization', 'mortality_rate', 'pop_density']
-X = data[features]
-y = data.death
+    features = ['age', 'male', 'female', 'number of days between symptom onset and hospitalization', 'mortality_rate', 'pop_density', 'high risk travel']
+    X = data[features]
+    y = data.death
 
-from xgboost import XGBRegressor
-model = XGBRegressor(n_estimators=100, learning_rate=0.05)
-model.fit(X, y)
+    from xgboost import XGBRegressor
+    model = XGBRegressor(n_estimators=100, learning_rate=0.05)
+    model.fit(X, y)
 
-from sklearn.model_selection import cross_val_predict
-from sklearn.metrics import roc_auc_score
+    from sklearn.model_selection import cross_val_predict
+    from sklearn.metrics import roc_auc_score
 
-predictions = cross_val_predict(model, X, y, cv=10)
-auc = roc_auc_score(y, predictions)
-print(auc)
+    predictions = cross_val_predict(model, X, y, cv=10)
+    auc = roc_auc_score(y, predictions)
+    #print(auc)
 
-# Scoring for mean absolute error
-#from sklearn.model_selection import cross_val_score
-#scores = -cross_val_score(model, X, y, scoring='neg_mean_absolute_error', cv=10)
-#print(scores.mean())
-
-
+    return model.predict(test_data)
