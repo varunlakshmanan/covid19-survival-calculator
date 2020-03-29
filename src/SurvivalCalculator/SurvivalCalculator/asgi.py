19eb46cd605db9abc0f10f5e6acf570712748375
computer-geek64 <sudouser512@gmail.com>
2020-03-29 17:47:28 -0400

    Update

--- src/SurvivalCalculator/SurvivalCalculator/asgi.py
+++ src/SurvivalCalculator/SurvivalCalculator/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for RecoveryCalculator project.
+ASGI config for SurvivalCalculator project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RecoveryCalculator.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SurvivalCalculator.settings')
 
 application = get_asgi_application()
--- src/SurvivalCalculator/SurvivalCalculator/wsgi.py
+++ src/SurvivalCalculator/SurvivalCalculator/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for RecoveryCalculator project.
+WSGI config for SurvivalCalculator project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RecoveryCalculator.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SurvivalCalculator.settings')
 
 application = get_wsgi_application()
