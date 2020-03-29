c63a4b520f96dd3cc0ca4d50d38a8ae553e07d69
varunlakshmanan <varunlakshmanan@outlook.com>
2020-03-29 19:21:19 -0400

    Updated model weighting

--- src/models/lgbm_model.py
+++ src/models/lgbm_model.py
@@ -3,7 +3,6 @@ def create_light_gradient_boosting_model(test_data):
     import pandas as pd
 
     data_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'datasets', 'data.csv')
-    #data_file_path = 'D:/Documents/Leetcode + Foobar + Kaggle/COVIDCalculator/updated_data.csv'
     data = pd.read_csv(data_file_path)
 
     features = ['age', 'male', 'female', 'symptom_onset_hospitalization', 'mortality_rate', 'pop_density', 'high_risk_travel']
@@ -14,17 +13,17 @@ def create_light_gradient_boosting_model(test_data):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state=0)
 
     import lightgbm as lgb
-    train_dataset = lgb.Dataset(X_train, label=y_train, params={'verbose': -1})
+    train_dataset = lgb.Dataset(X_train, label=y_train)
     param = {'num_leaves': 64,
              'objective': 'binary',
              'learning_rate': 0.005,
              'boosting_type': 'dart',
              'max_depth': 8,
-             'num_iterations': 500,
-             'min_data_in_leaf': 55,
+             'num_iterations': 2500,
+             'min_data_in_leaf': 40,
              'metric': 'auc',
              'verbose': -1}
-    bst = lgb.train(param, train_dataset, 100)
+    bst = lgb.train(param, train_dataset, 100, verbose_eval=False)
 
     from sklearn.metrics import roc_auc_score
     predictions = bst.predict(X_test, num_iteration=bst.best_iteration)
