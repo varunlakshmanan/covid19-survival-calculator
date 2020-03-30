b0ed2ff3ae0fdaeedae012c3fe6896849f60586b
varunlakshmanan <varunlakshmanan@outlook.com>
2020-03-29 22:51:50 -0400

    Comments for clarity

--- src/models/lgbm_model.py
+++ src/models/lgbm_model.py
@@ -6,15 +6,19 @@ from sklearn.model_selection import train_test_split
 
 
 def create_light_gradient_boosting_model(test_data):
+    # Read data
     data_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'datasets', 'data.csv')
     data = pd.read_csv(data_file_path)
 
+    # Set model features and prediction target
     features = ['age', 'male', 'female', 'symptom_onset_hospitalization', 'mortality_rate', 'pop_density', 'high_risk_travel']
     x = data[features]
     y = data.death
 
+    # Split data into train and test sets
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state=0)
 
+    # Train model 
     train_dataset = lgb.Dataset(x_train, label=y_train)
     param = {'num_leaves': 64,
              'objective': 'binary',
@@ -27,6 +31,7 @@ def create_light_gradient_boosting_model(test_data):
              'verbose': -1}
     bst = lgb.train(param, train_dataset, 100, verbose_eval=False)
 
+    # Calculate AUC value 
     # predictions = bst.predict(x_test, num_iteration=bst.best_iteration)
     # auc = roc_auc_score(y_test, predictions)
     # print(auc)
